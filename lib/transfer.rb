876fde1eb58bae935179f99bd29e7b8fa665f1b3
Nethelbert <gilblasse@gmail.com>
2019-11-16 01:58:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
- 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(transfer)
+    @transfer = transfer
+    @balance = 1000
+    @status = "open"
+  end
 end
